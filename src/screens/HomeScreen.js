d504717e5be30797106b1f0d423920947336ced8
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-10-09 15:29:03 -0600

    Main | Creando bd en app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,32 @@
 import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase({ name: 'miBD.db' });
+
+const crearTablaVentas = () => {
+  db.transaction(tx => {
+    tx.executeSql(`
+      CREATE TABLE IF NOT EXISTS ventas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        venta TEXT NOT NULL,
+        fecha_venta DATE NOT NULL
+      );
+    `, [], (tx, results) => {
+      console.log('Tabla creada con éxito');
+    }, error => {
+      console.error('Error al crear la tabla:', error);
+    });
+  });
+};
 
 export const  HomeScreen  = () => {
+
+  useEffect(() => {
+    crearTablaVentas();
+  }, []);
+
+
     return (
       <View style={{flex:1, alignContent:'center', alignItems:'center', justifyContent:'center'}}>
         <Text style={{fontSize:20, fontWeight:'400'}}>Aplicación para el registro de ventas de bebidas en la escuela.</Text>
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,11 +1,76 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import {Alert, Modal, StyleSheet, Text, Pressable, View, TextInput} from 'react-native';
+import React, { useState }from 'react'
+import DateTimePicker from 'react-native-ui-datepicker';
+import dayjs from 'dayjs';
+import { Button, Input, Overlay } from 'react-native-elements';
 
 export const  RegisterScreen  = () => {
+
+  const [date, setDate] = useState(dayjs());
+  const [modalVisible, setModalVisible] = useState(true)
+  const [number, onChangeNumber] = React.useState('');
+
     return (
-      <View>
-        <Text>RegisterScreen</Text>
+      <View style={{marginHorizontal:10}}>
+        <View style={{backgroundColor:'#fff', marginTop:20, borderRadius:10, marginBottom:20}}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder=" Ingrese la cantidad vendida el dia de hoy..."
+            keyboardType="numeric"
+          />
+        </View>
+        
+        <Button
+          title="Registrar venta"
+        />
       </View>
+      
     )
     
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
